b18361ac8632dd1155b4d465b5363ccac178c20d
Sunil Pandit <sunil.pandit@optym.com>
2025-11-29 21:11:10 +0530

    removed unnecessary file

--- supabase/migration.sql
+++
@@ -1,9 +0,0 @@
--- Migration script to fix duplicate constraint issue
--- Run this if you get "constraint already exists" error
-
--- Drop the constraint if it exists (in case table was partially created)
-ALTER TABLE IF EXISTS messages DROP CONSTRAINT IF EXISTS messages_role_check;
-
--- Now the main schema can be run safely
--- The constraint is defined inline in the CREATE TABLE statement
-
